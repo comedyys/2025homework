56cc3bd911655e5b361fff865147658b844e66c2
Ayanami Rei <1053207435@qq.com>
2025-06-01 15:43:12 +0800

    second commit

---
+++ demo2.py
@@ -0,0 +1,15 @@
+import datetime
+
+def NextDate(year, month, day):
+    try:
+        current_date = datetime.date(year, month, day)
+        next_date = current_date + datetime.timedelta(days=1)
+        return next_date.strftime("%Y-%m-%d")
+    except ValueError:
+        return "Invalid date"
+
+
+print(NextDate(2025, 6, 1))  
+print(NextDate(2025, 12, 31))  
+print(NextDate(2025, 2, 28))  
+print(NextDate(2024, 2, 28))  
